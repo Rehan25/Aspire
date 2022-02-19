800fd57d91816a95888d874635b3b5227e179ae3
rtech <rtech@192.168.0.6>
2022-02-19 22:39:32 +0530

    My first commit

---
+++ Aspire2022/src/accessDemo1/AccessDemo1.java
@@ -0,0 +1,18 @@
+package accessDemo1;
+
+public class AccessDemo1 {
+	
+		private int pri;
+		int nomod;
+		protected int pro;
+		public int pub;
+		
+		void met() {
+			System.out.println(pri);
+			System.out.println(nomod);
+			System.out.println(pro);
+			System.out.println(pub);
+
+}
+
+}
---
+++ Aspire2022/src/aspire22/ReturnMethods.java
@@ -0,0 +1,51 @@
+package aspire22;
+	
+	//RETURN METHODS - https://fluvid.com/videos/detail/QKZX3hd-aaURvYYn-#.YgxlG7V6aWA.link
+	public class ReturnMethods {
+		public static void main(String[] args) {
+			ReturnMethods obj=new ReturnMethods();
+			
+			int a=obj.call();
+			
+			System.out.println(a);
+			
+			
+			System.out.println(obj.call(1));
+			
+			System.out.println(obj.getName(100));
+		}
+		
+		int call() {
+			return 100;
+		}
+		
+		short call(int i) {
+			if(i==10) {
+				return 10;
+			}
+			else {
+				return 100;
+			}
+		}
+		
+		boolean run() {
+			return true;
+		}
+		
+		String getName(int rollno) {
+			switch(rollno) {
+			case 100:{
+				return "abdulllah";
+				//break;
+			}
+			case 200:{
+				return "amar";
+				//break;
+			}
+			default:{
+				return "noman";
+			}
+			}
+		}
+	}
+
